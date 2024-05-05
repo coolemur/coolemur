e06d3124ed5f964a22ca76de6abcd4828278aac6
Mantas <mantas.janusauskas@gmail.com>
2024-05-05 11:36:06 +0200

    .

--- src/routes/design/color.svelte
+++ src/routes/design/color.svelte
@@ -1,8 +1,23 @@
 <script>
-  const { name } = $$props;
+  import { cva } from 'class-variance-authority';
+  const { name } = $props();
+
+  const color = cva('flex items-center justify-center rounded-lg text-4xl capitalize text-white', {
+    variants: {
+      color: {
+        highlight: 'bg-highlight',
+        'highlight-dark': 'bg-highlight-dark',
+        paper: 'bg-paper',
+        'paper-light': 'bg-paper-light',
+        'dark-gray': 'bg-dark-gray',
+        secondary: 'bg-secondary',
+        'secondary-dark': 'bg-secondary-dark'
+      }
+    }
+  });
 </script>
 
-<div class="bg-{name} flex items-center justify-center rounded-lg text-4xl capitalize text-white">
+<div class={color({ color: name })}>
   <span class="rounded-lg bg-black/20 py-2 px-4">
     {name}
   </span>
